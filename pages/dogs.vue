fa5406c086a7cc78c40356c8899fe2f3dd8c01db
Leenur Alonen <923995@ACDM.DS.SAIT.CA>
2024-01-20 13:53:35 -0700

    typed out dogs.vue

---
+++ pages/dogs.vue
@@ -0,0 +1,18 @@
+<script setup>
+const {data} = await useFetch ("api/dogs");
+const breeds = data;
+console.log(breeds);
+</script>
+<template>
+  <main>
+    <header> <h1>Dog Breeds</h1> </header>
+      <div>
+        <u1> 
+          <li v-for="breed in breeds" :key="breed.id">
+          {{ breed }}
+          </li>
+        </u1>
+      </div>
+    
+  </main>
+</template>
