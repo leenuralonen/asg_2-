8eb9049090d75a1627b9e4817b50c79455ef9378
Leenur Alonen <923995@ACDM.DS.SAIT.CA>
2024-01-20 14:18:32 -0700

    typed out employees.vue

---
+++ pages/employees.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+const { data } = await useFetch("/api/employees", {
+  headers: useRequestHeaders(["cookie"]),
+});
+const employees = data._value.employees;
+</script>
+
+<template>
+  <main>
+    <h1>Employees</h1>
+    <!-- render the fetched data -->
+    <ul>
+      <li v-for="employee in employees" :key="employee.EMPLOYEE_ID">
+        <h3>Name: {{ employee.FIRST_NAME }} {{ employee.LAST_NAME }}</h3>
+        <p>Email: {{ employee.EMAIL }}</p>
+        <p>Phone: {{ employee.PHONE_NUMBER }}</p>
+      </li>
+    </ul>
+  </main>
+</template>
