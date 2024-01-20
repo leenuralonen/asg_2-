fc291284bdb741b5bd2df7da71387eddba3f0f37
Leenur Alonen <923995@ACDM.DS.SAIT.CA>
2024-01-20 13:44:16 -0700

    Typed out Appheader

---
+++ components/AppHeader.vue
@@ -0,0 +1,37 @@
+<template>
+  <header>
+    <div>
+      <NuxtLink to="/">Logo: User Name</NuxtLink>
+    </div>
+    <nav>
+      <NuxtLink to="employees">Employees</NuxtLink>
+      <nuxtLink to="dogs">Dogs</nuxtLink>
+      <nuxtLink to="signup">Sign Up Page</nuxtLink>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+header {
+display: flex;
+justify-content: space-between;
+gap: 2rem;
+padding: 2rem;
+color: whitesmoke;
+background-color: blue;
+}
+a {
+  color: white;
+  text-decoration: mediumpurple;
+}
+a:hover {
+  color:blue;
+
+}
+nav{
+  display: flex;
+  justify-content: space-between;
+  gap: 12px
+}
+
+</style>
