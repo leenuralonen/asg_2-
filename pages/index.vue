ef2e91118f4ea8364932d16b279dd4f73352707d
Leenur Alonen <923995@ACDM.DS.SAIT.CA>
2024-01-20 14:40:49 -0700

    typed index

---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<script setup>
+const { data: hello } = await useFetch("/api/hello");
+const { data } = await useFetch("/api/disney");
+const characters = data;
+const clientSideExample = "Updated check";
+</script>
+
+<template>
+  <main>
+    <header>
+      <h1>Hello CPRG</h1>
+    </header>
+
+    <section>
+      <h2>Hello Data Endpoint Example</h2>
+      <p>{{ hello.hello }}</p>
+      {{ clientSideExample }}
+    </section>
+    <pre>{{ characters }}</pre>
+    <section>
+      <h2>Disney Character Fetch</h2>
+      <ul>
+        <li v-for="character in characters.data" :key="character.id">
+          <h3>{{ character.name }}</h3>
+          <img :src="character.imageUrl" />
+        </li>
+      </ul>
+    </section>
+  </main>
+</template>
